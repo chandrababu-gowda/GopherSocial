ddcb6fd3a76447a6453b1aae5189f21f4e287ce8
chandrababu-gowda <chandrababugowda3042003@gmail.com>
2025-12-29 16:39:43 +0530

    feat: make the user and post service

---
+++ internal/store/storage.go
@@ -0,0 +1,35 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+)
+
+var (
+	ErrNotFound          = errors.New("Resouce not found")
+	ErrConflict          = errors.New("Resource already exists")
+	QueryTimeoutDuration = time.Second * 5
+)
+
+type Storage struct {
+	Posts interface {
+		GetByID(ctx context.Context, id int64) (*Post, error)
+		Create(ctx context.Context, post *Post) error
+		Delete(ctx context.Context, postID int64) error
+		Update(ctx context.Context, post *Post) error
+		GetUserFeed(ctx context.Context, userID int64, fq PaginatedFeedQuery) ([]PostWithMetadata, error)
+	}
+	Users interface {
+		Create(context.Context, *User) error
+		GetByID(context.Context, int64) (*User, error)
+	}
+}
+
+func NewStorage(db *sql.DB) Storage {
+	return Storage{
+		Posts: &PostStore{db},
+		Users: &UserStore{db},
+	}
+}
