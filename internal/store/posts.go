1fb8a238455be5a6fed196d191a7c1d80fd9fb5c
chandrababu-gowda <chandrababugowda3042003@gmail.com>
2025-12-30 13:08:30 +0530

    setup: make the post handlers

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -8,17 +8,16 @@ import (
 )
 
 type Post struct {
-	ID        int64    `json:"id"`
-	Content   string   `json:"content"`
-	Title     string   `json:"title"`
-	UserID    int64    `json:"user_id"`
-	Tags      []string `json:"tags"`
-	CreatedAt string   `json:"created_at"`
-	UpdatedAt string   `json:"updated_at"`
-	Version   int      `json:"version"`
-	// TODO: After creating comments.go change the type to []Comment
-	Comments []string `json:"comments"`
-	User     User     `json:"user"`
+	ID        int64     `json:"id"`
+	Content   string    `json:"content"`
+	Title     string    `json:"title"`
+	UserID    int64     `json:"user_id"`
+	Tags      []string  `json:"tags"`
+	CreatedAt string    `json:"created_at"`
+	UpdatedAt string    `json:"updated_at"`
+	Version   int       `json:"version"`
+	Comments  []Comment `json:"comments"`
+	User      User      `json:"user"`
 }
 
 type PostWithMetadata struct {
